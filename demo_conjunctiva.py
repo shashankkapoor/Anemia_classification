b8c0c9256483ec54bf2a0b4016ab34378a3e6f58
shashankkapoor <ubuntu@ip-172-31-11-66.ap-south-1.compute.internal>
2024-02-07 17:25:37 +0000

    demo conj. file added

---
+++ demo_conjunctiva.py
@@ -0,0 +1,91 @@
+import os
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+
+import cv2
+from tensorflow import keras
+
+# from tensorflow.keras.utils import img_to_array
+import numpy as np
+import flask
+from PIL import Image
+import io
+
+# initialize our Flask application and the Keras model
+app = flask.Flask(__name__)
+model = None
+
+
+def create_opencv_image_from_stringio(img_stream, cv2_img_flag=0):
+    # img_stream.seek(0)
+    img_array = np.asarray(bytearray(img_stream), dtype=np.uint8)
+    return cv2.imdecode(img_array, cv2_img_flag)
+
+
+# preprocess image
+def process_img(img) -> np.array:
+    # img = cv2.imread(filepath)
+    # img = img_to_array(img)
+    img = cv2.resize(img, (64, 64))
+    return img
+
+
+# get model
+def get_efficientnetv2b0():
+    
+    cnn = keras.applications.EfficientNetV2B0(
+        include_top=False,
+        input_shape=(64, 64, 3)
+    )
+    cnn.trainable = True
+
+    model = keras.models.Sequential()
+    model.add(cnn)
+    model.add(keras.layers.GlobalAveragePooling2D())
+    model.add(keras.layers.Dense(1024, activation='relu'))
+    model.add(keras.layers.Dense(1024, activation='relu'))
+    model.add(keras.layers.Dense(2, activation='softmax'))
+
+    model.load_weights("weights_conjunctiva/weights/efficientnetV2B0_model.h5", skip_mismatch=False, by_name=False, options=None)
+    return model
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    # initialize the data dictionary that will be returned from the view
+    data = {"success": False}
+
+    # ensure an image was properly uploaded to our endpoint
+    if flask.request.method == "POST":
+        if flask.request.files.get("image"):
+            # read the image in PIL format
+            image = flask.request.files["image"].read()
+            image = create_opencv_image_from_stringio(image, cv2.IMREAD_COLOR)
+            # image = Image.open(io.BytesIO(image))
+            # preprocess the image and prepare it for classification
+            image = process_img(image)
+            image = image[None, :, :, :3]
+            model.trainable = False
+            pred = model.predict(image, verbose=0)
+            data["prob"] = [float(pred[0, 0]), float(pred[0, 1])]
+
+            pred = np.argmax(pred, axis=1)  # bad practice, will change later
+            if pred[0] == 0:
+                data["anemia"] = True
+            if pred[0] == 1:
+                data["anemia"] = False
+
+            data["success"] = True
+    return flask.jsonify(data)
+
+
+if __name__ == "__main__":
+    print(
+        (
+            "* Loading Keras model and Flask starting server..."
+            "please wait until server has fully started"
+        )
+    )
+    model = get_efficientnetv2b0()
+    print("Model loaded with weights. ")
+    app.run()
