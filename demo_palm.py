ae4841a1b3cd9942eac900fc2d2c5be7295b937f
raunav <raunavghosh@Raunavs-MacBook-Air.local>
2024-02-08 17:39:13 +0530

    debug=False

--- demo_palm.py
+++ demo_palm.py
@@ -99,4 +99,4 @@ if __name__ == "__main__":
     )
     model = get_convnexttiny()
     print("Model loaded with weights. ")
-    app.run(host='0.0.0.0', debug=True, port=5000)
+    app.run(host='0.0.0.0', debug=False, port=5000)
